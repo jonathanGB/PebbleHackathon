db8db7169048b0c97846f4acf87c7cb3446c4848
Ubuntu <drumyup101@hotmail.com>
2016-04-02 19:20:36 +0000

    ssl cert installed and working

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-var express = require('express');
-var app = express();
+const   express = require('express'),
+	fs = require('fs'),
+	https = require('https');
+
+var     app = express(),
+	port = 443;
+
+var certOptions = {
+	ca: fs.readFileSync('../pebble-localize_win.ca-bundle'),
+	key: fs.readFileSync('../pebble-localize-ssl.pem'),
+	cert: fs.readFileSync('../pebble-localize_win.crt')
+};
+
+var server = https.createServer(certOptions, app).listen(port, () => {
+	console.log("Server started");
+})
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
-});
-
-app.listen(80, function () {
-  console.log('Example app listening on port 443!');
 });
