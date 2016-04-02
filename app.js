c36a6520e680eb9df87d901847668a4802c866a7
Ubuntu <drumyup101@hotmail.com>
2016-04-02 22:24:52 +0000

    progress routing

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const   express = require('express'),
 	https = require('https');
 
 var     app = express(),
-	port = 443;
+	port = 443,
+	MongoClient = require('mongodb').MongoClient,
+	dbUrl = 'mongodb://localhost:27017/localizeDB';
 
 var certOptions = {
 	ca: fs.readFileSync('../pebble-localize_win.ca-bundle'),
@@ -17,4 +19,31 @@ var server = https.createServer(certOptions, app).listen(port, () => {
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+})
+.post('/user/join', function(req, res) {
+	MongoClient.connect(dbUrl, function(err, db) {
+		if (err) throw err;
+		
+		var     name = sanitizeInput(req.query.name),
+			username = sanitizeInput(req.query.username),
+			pword = sanitizeInput(req.query.pword);
+		
+		console.log(name,  ' / ', username, ' / ', pword);		
+		if (!name || !username || !pword)
+			res.status(400).send('Bad input');
+		else if (db.collection('users').findOne({'username': username}) != null)
+			res.status(403).send('Username already exists');
+		else {
+			db.collection('users').insertOne({'username': username, 'name': name, 'pword': pword}, function(errInsert, result) {
+				if (errInsert)
+					res.status(500).send('Error creating user');
+				else
+					res.status(200).send('JOIN OK');
+			});
+		}
+	});
 });
+
+function sanitizeInput(input) {
+	return input.replace(/[\\$\[\]^\(\)\*&%#@!\/<>\?\+=:;\|"]/gi, '');
+}
